4ab36fed233750eb874b439a8fe6734c1214ed4a
Y_F_TONKATSU <machbuilders@gmail.com>
2018-05-03 22:18:53 +0900

    Setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+
+var _ = require('lodash');
+
+var minify = require('gulp-minify');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
+
+var fs = require('fs');
+var path = require('path');
+
+var PATH_BASE_DEST = 'D:/Arts/Web/tnode/tsnake/public/'
+
+var getScriptSrc = function () {
+    return [PATH_BASE_DEST + 'scripts/**/*.js']
+};
+
+var getScriptDest = function () {
+    return PATH_BASE_DEST + 'scripts_dest/'
+};
+
+gulp.task('bundle_js', function () {
+
+    return gulp.src(getScriptSrc())
+        .pipe(concat('bundle.js'))
+        .pipe(minify())
+        .pipe(gulp.dest(getScriptDest()));
+});
+
+gulp.task('clear_script', function () {
+    return gulp.src(path.join(getScriptDest(), '*.js'), {read: false})
+        .pipe(clean());
+});
+
+gulp.task('tsnake', function () {
+    gulp.start(['clear_script', 'bundle_js']);
+});
+
+gulp.task('default', ['tsnake']);
---
+++ routes/main.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('main', {
+        og_title: 'TSnake',
+        og_description: 'TSnake',
+        og_url: 'http://knights.ton-katsu.net/apps/tsnake/',
+        og_thumb: 'images/og_thumb.png',
+    });
+});
+
+module.exports = router;
