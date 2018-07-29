20d50596320088161fbca3cfc8cec8b06c3522a4
Y_F_TONKATSU <machbuilders@gmail.com>
2018-07-30 01:32:58 +0900

    Fix ENVE_VAR trick

--- routes/main.js
+++ routes/main.js
@@ -5,10 +5,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('main', {
-        base_url: 'http://knights.ton-katsu.net/apps/tsnake/',
+        base_url: 'http://knights.ton-katsu.net/games/tsnake/',
         og_title: 'TSnake',
         og_description: 'TSnake',
-        og_url: 'http://knights.ton-katsu.net/apps/tsnake/',
+        og_url: 'http://knights.ton-katsu.net/games/tsnake/',
         og_thumb: 'images/og_thumb.png',
     });
 });
