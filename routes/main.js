c6d2efcd8ce13895c883ab85473e9c52db1df00a
Y_F_TONKATSU <machbuilders@gmail.com>
2018-07-30 01:26:11 +0900

    Fix ENVE_VAR trick

--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var baseUrl;
-if(process.env.UNKO == 'local'){
-    baseUrl = '';
-} else if (process.env.UNKO == 'remote') {
-    baseUrl = 'http://knights.ton-katsu.net/apps/tsnake/'
-}
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('main', {
-        base_url: baseUrl,
+        base_url: process.env.UNKO == 'remote' ? 'http://knights.ton-katsu.net/apps/tsnake/' : '',
         og_title: 'TSnake',
         og_description: 'TSnake',
         og_url: 'http://knights.ton-katsu.net/apps/tsnake/',
