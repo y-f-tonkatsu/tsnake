9e33351448b4de352af2d9319d1a27be6d835e0c
Y_F_TONKATSU <machbuilders@gmail.com>
2018-07-30 01:21:49 +0900

    Add ENVE_VAR trick

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var users = require('./routes/score');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var baseUrl;
+if(process.env.UNKO == 'local'){
+    baseUrl = '';
+} else if (process.env.UNKO == 'remote') {
+    baseUrl = 'http://knights.ton-katsu.net/apps/tsnake/'
+}
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('main', {
+        base_url: baseUrl,
         og_title: 'TSnake',
         og_description: 'TSnake',
         og_url: 'http://knights.ton-katsu.net/apps/tsnake/',
