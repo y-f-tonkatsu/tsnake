2e1db721ae4f455942d7b6772d28bb92e8198bbb
Y_F_TONKATSU <machbuilders@gmail.com>
2018-11-20 22:00:16 +0900

    Add Areas

--- public/scripts/areas/areas.js
+++ public/scripts/areas/areas.js
@@ -82,6 +82,246 @@ var Areas;
             ],
             "initialSpeed": 4
         },
+
+        //3
+        {
+            "items": [
+                {
+                    "id": "Apple",
+                    "spawnRate": 0.2,
+                },
+                {
+                    "id": "Berry",
+                    "spawnRate": 0.1,
+                },
+                {
+                    "id": "Wine",
+                    "spawnRate": 0.03,
+                }
+            ],
+            "dropItems": [
+                {
+                    "id": "Berry",
+                    "dropRate": 0.8,
+                },
+                {
+                    "id": "Coin",
+                    "dropRate": 0.7,
+                },
+                {
+                    "id": "Key",
+                    "dropRate": 0.9,
+                }
+            ],
+            "enemies": [
+                {
+                    "id": "Frog",
+                    "spawnRate": 0.8,
+                    "dropItemRate": 0.4
+                }
+            ],
+            "initialSpeed": 4
+        },
+
+        //4
+        {
+            "items": [
+                {
+                    "id": "Apple",
+                    "spawnRate": 0.2,
+                },
+                {
+                    "id": "Berry",
+                    "spawnRate": 0.1,
+                },
+                {
+                    "id": "Wine",
+                    "spawnRate": 0.03,
+                }
+            ],
+            "dropItems": [
+                {
+                    "id": "Berry",
+                    "dropRate": 0.8,
+                },
+                {
+                    "id": "Coin",
+                    "dropRate": 0.7,
+                },
+                {
+                    "id": "Key",
+                    "dropRate": 0.9,
+                }
+            ],
+            "enemies": [
+                {
+                    "id": "Frog",
+                    "spawnRate": 0.8,
+                    "dropItemRate": 0.4
+                }
+            ],
+            "initialSpeed": 4
+        },
+
+        //5
+        {
+            "items": [
+                {
+                    "id": "Apple",
+                    "spawnRate": 0.2,
+                },
+                {
+                    "id": "Berry",
+                    "spawnRate": 0.1,
+                },
+                {
+                    "id": "Wine",
+                    "spawnRate": 0.03,
+                }
+            ],
+            "dropItems": [
+                {
+                    "id": "Berry",
+                    "dropRate": 0.8,
+                },
+                {
+                    "id": "Coin",
+                    "dropRate": 0.7,
+                },
+                {
+                    "id": "Key",
+                    "dropRate": 0.9,
+                }
+            ],
+            "enemies": [
+                {
+                    "id": "Frog",
+                    "spawnRate": 0.8,
+                    "dropItemRate": 0.4
+                }
+            ],
+            "initialSpeed": 4
+        },
+
+        //6
+        {
+            "items": [
+                {
+                    "id": "Apple",
+                    "spawnRate": 0.2,
+                },
+                {
+                    "id": "Berry",
+                    "spawnRate": 0.1,
+                },
+                {
+                    "id": "Wine",
+                    "spawnRate": 0.03,
+                }
+            ],
+            "dropItems": [
+                {
+                    "id": "Berry",
+                    "dropRate": 0.8,
+                },
+                {
+                    "id": "Coin",
+                    "dropRate": 0.7,
+                },
+                {
+                    "id": "Key",
+                    "dropRate": 0.9,
+                }
+            ],
+            "enemies": [
+                {
+                    "id": "Frog",
+                    "spawnRate": 0.8,
+                    "dropItemRate": 0.4
+                }
+            ],
+            "initialSpeed": 4
+        },
+
+        //7
+        {
+            "items": [
+                {
+                    "id": "Apple",
+                    "spawnRate": 0.2,
+                },
+                {
+                    "id": "Berry",
+                    "spawnRate": 0.1,
+                },
+                {
+                    "id": "Wine",
+                    "spawnRate": 0.03,
+                }
+            ],
+            "dropItems": [
+                {
+                    "id": "Berry",
+                    "dropRate": 0.8,
+                },
+                {
+                    "id": "Coin",
+                    "dropRate": 0.7,
+                },
+                {
+                    "id": "Key",
+                    "dropRate": 0.9,
+                }
+            ],
+            "enemies": [
+                {
+                    "id": "Frog",
+                    "spawnRate": 0.8,
+                    "dropItemRate": 0.4
+                }
+            ],
+            "initialSpeed": 4
+        },
+
+        //8
+        {
+            "items": [
+                {
+                    "id": "Apple",
+                    "spawnRate": 0.2,
+                },
+                {
+                    "id": "Berry",
+                    "spawnRate": 0.1,
+                },
+                {
+                    "id": "Wine",
+                    "spawnRate": 0.03,
+                }
+            ],
+            "dropItems": [
+                {
+                    "id": "Berry",
+                    "dropRate": 0.8,
+                },
+                {
+                    "id": "Coin",
+                    "dropRate": 0.7,
+                },
+                {
+                    "id": "Key",
+                    "dropRate": 0.9,
+                }
+            ],
+            "enemies": [
+                {
+                    "id": "Frog",
+                    "spawnRate": 0.8,
+                    "dropItemRate": 0.4
+                }
+            ],
+            "initialSpeed": 4
+        },
     ];
 
 })();
