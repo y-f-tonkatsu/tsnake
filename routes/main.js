fc8ba0d5005a29817a324206d54408a49189def4
Y_F_TONKATSU <machbuilders@gmail.com>
2019-01-26 16:55:35 +0900

    Add ex

--- routes/main.js
+++ routes/main.js
@@ -6,7 +6,7 @@ router.get('/', function (req, res, next) {
     res.render('main', {
         base_url: process.env.UNKO == "remote" ? 'http://knights.ton-katsu.net/games/tsnake/' : '',
         og_title: "YFT's TSnake",
-        og_description: 'クラシックなスネークゲームをアレンジしたシンプルなゲームです。',
+        og_description: 'クラシックなスネークゲームをアレンジしたシンプルなゲームです。JavaScript と Adobe Animate で作成。',
         og_url: 'http://knights.ton-katsu.net/games/tsnake/',
         og_thumb: 'http://knights.ton-katsu.net/games/tsnake/images/ss_og.jpg',
     });
